48e4c44c4e0f4749868c4d3aabba17998a97b9a8
chellaler-dev <chellaler.dev@gmail.com>
2026-03-26 23:01:21 +0300

    fix: update S3 configuration to use correct temporary URL environment variable

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,7 +58,7 @@ return [
             'region' => env('S3_REGION'),
             'bucket' => env('S3_BUCKET'),
             'url' => env('S3_URL'),
-            'temporary_url' => env('S3_URL'),
+            'temporary_url' => env('S3_TEMPORARY_URL'),
             'endpoint' => env('S3_ENDPOINT'),
             'use_path_style_endpoint' => env('S3_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => true,
